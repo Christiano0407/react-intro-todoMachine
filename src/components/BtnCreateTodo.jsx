86088e3ee017540db755a3be3f00e3209bb02291
Christiano0407 <placebo0704@gmail.com>
2023-05-25 08:31:14 -0600

    add plugins and responsive design that tailwindcss

--- src/components/BtnCreateTodo.jsx
+++ src/components/BtnCreateTodo.jsx
@@ -3,7 +3,7 @@
 export function BtnCreateTodo() {
   return (
     <>
-      <button className="p-[1rem] font-medium text-[1.5rem] text-white bg-[#333] mt-8 rounded-lg outline-none ">
+      <button className="p-[1rem] font-medium text-[1.5rem] text-white bg-black mt-8 rounded-lg outline-none ">
         Click
       </button>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,27 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      mob: { min: '320px', max: '639px' },
+
+      sm: { min: '640px', max: '767px' },
+      // => @media (min-width: 640px) { ... } {'min': '640px', 'max': '767px'},
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontFamily: {
+      sans: ['Roboto', `sans-serif`],
+    },
   },
   plugins: [],
 };
