1b36e1a599309187638a74dde90e758d0c32ecde
Christiano0407 <placebo0704@gmail.com>
2023-05-29 15:56:33 -0600

    new changes with state

--- src/components/BtnCreateTodo.jsx
+++ src/components/BtnCreateTodo.jsx
@@ -4,7 +4,14 @@ import { VscAdd } from "react-icons/vsc";
 export function BtnCreateTodo() {
   return (
     <>
-      <button className="w-[80%] p-[1.5rem] mx-auto my-0 bg-[#333] text-white flex items-center justify-center rounded-2xl active:scale-90">
+      <button
+        className="w-[80%] p-[1.5rem] mx-auto my-0 bg-[#333] text-white flex items-center justify-center rounded-2xl active:scale-90"
+        onClick={(e) => {
+          e.preventDefault();
+          console.log("Click On Btn");
+          console.log(e.target);
+        }}
+      >
         <VscAdd />
       </button>
     </>
--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,12 +1,21 @@
 //**? =================== Component Todo Search ================= */
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export function TodoSearch() {
+  const [state, setState] = useState("");
+
+  console.log("Changes add" + state);
+
   return (
     <div className=" w-[90%] p-[1rem] flex items-center mx-auto my-2 border-solid border-2 border-black rounded-[5rem] font-semibold text-lg ">
       <input
         placeholder="Call List"
         className="p-[1rem] w-[90%] outline-none border-none"
+        value={state}
+        onChange={(e) => {
+          setState(e.target.value);
+        }}
       ></input>
       <a className="ml-2">
         <FaSearch />
