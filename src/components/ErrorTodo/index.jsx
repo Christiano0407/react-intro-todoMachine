4d9630be500b488be8eb1c82b450485918542373
Christiano0407 <placebo0704@gmail.com>
2023-06-07 10:00:58 -0600

    changes al process css on lazyload

--- src/components/ErrorTodo/index.jsx
+++ src/components/ErrorTodo/index.jsx
@@ -3,10 +3,10 @@ import "./lazyError.css";
 export const ErrorTodo = () => {
   return (
     <section className="w-[80%] bg-white border-[5px] border-solid border-[#e5e6e9] rounded-md p-3 my-0 mx-auto mb-3">
-      <p className="font-bold text-[#333] text-2xl">
+      <p className="font-bold text-[#333] text-2xl mb-4">
         Error Change and Add Todos
       </p>
-      <div className="min-h-full bg-[#f6f7f8] bg-gradient-[to right, #eeeeee 8%, #dddddd 18%, #eeeeee 33%] flex background"></div>
+      <div className="min-h-full bg-[#f6f7f8] bg-gradient-[to right, #eeeeee 8%, #dddddd 18%, #eeeeee 33%] flex"></div>
     </section>
   );
 };
